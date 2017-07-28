dbab2f891f1e0d0bad45d3df2e3ffe7ad0785ce1
DESKTOP-UFN46JE\Petrus <petrus.prinsloo@gmail.com>
2017-07-28 20:59:43 +0200

    Working on start state.

---
+++ City/game_state_editor.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef GAME_STATE_EDITOR_HPP
+#define GAME_STATE_EDITOR_HPP
+
+#include <SFML/Graphics.hpp>
+
+#include "game_state.h"
+
+class GameStateEditor : public GameState
+{
+private:
+	sf::View gameView;
+	sf::View guiView;
+
+public:
+	virtual void draw(const float dt);
+	virtual void update(const float dt);
+	virtual void handleInput();
+
+	GameStateEditor(Game* game);
+};
+
+#endif // !GAME_STATE_EDITOR_HPP
